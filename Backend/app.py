ff8711367a0fef59e2ebf769eec702b3d9248524
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-07-22 13:22:26 +0530

    chat save in database and trying fine tuning

--- Backend/app.py
+++ Backend/app.py
@@ -2,13 +2,13 @@ import os
 from flask import Flask, request, jsonify
 import openai
 from dotenv import load_dotenv
-from flask_cors import CORS    # Import the CORS extension
 from pymongo import MongoClient
 import bcrypt
 import jwt
 from datetime import datetime, timedelta
 from middleware.middlewares import check_login_status 
 from config.db import get_mongo_client
+from flask_cors import CORS
 
 
 
@@ -23,7 +23,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 client = get_mongo_client()
 db = client["Chatbot"]  # Replace "your_database_name" with your actual database name
-users = db["user"]  # Replace "your_collection_name" with your actual collection name
+users = db["user"]
+chats = db['chats']
+prompts = db['prompt']  # Replace "your_collection_name" with your actual collection name
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -101,81 +103,92 @@ def home():
     return jsonify({"msg": "Home page"})
 
 
-@app.route('/parent', methods=['POST'])
-def generate_Pinfluencer():
-    keyword = request.json.get('keyword')
-    print(keyword)
-    
-    try:
-        def run_prompt():
-            prompt = f"Act as parenting influencer. You provide solutions to all parenting problems, you have to answer in the language in which the user asks you the question. While answering to them reply as human not machine try to replicate the tone of user.\n\nPrompt: My daughter is 19 mnths. But jab usko me koi chij nai deti hu ya uski koi zid Puri nai kti hu tou wo mujhe hit karti haiShe is just 19 mnths..how can I control this behaviourYa kabhi kabhi wo masti me b mujhe hit kar deti hai.I tell her hitting noo..nd wo khud b bolti hai hitting nooo..but not regularly..but spcly wen i dont listen to her. \nAnswer: Namaste sir - Aapki beti choti hai. Is umar mein kuch na milne pe kaise behave karna hai bachon ko pata nahin hot. Emotion pe kaabu nahin hota. Lekin bachon ka bhi maarna rok sakte hai. Thoda time laga ke. Kabhi bhi jiss cheez ke live bacha hit kar raha hai woh puri nahin karni kyonki phir bachey ko lagta hai ke maarne se cheez milegi. So a no means a no. But pyaar se. Aap calm aawaaz mein usko bol sakti hai - No using hands and feet. Mujhe lagti hai. Same line hi humein baar baar use karni hai. Phir Aap ski feeling ko acknowledge karo. Ke aapko woh chahiye. Haan? Mujhe pata hai. Mujhe pata hai aapko aacha lagta hai. Lekin maarne se kabhi nahin milega. Aur pun nai kti hu tou Mummy loves you. Ya kabhi kabhi wo Bachon ke nervous system ko touch karke se calmness milti hai. Unko touch karke pyaar se mana karenge to baat samajne ka chance zada hai. Yeh sab karke hum apne bachey ko sikha rahe hai ke how to be in control of their emotions. Yeh important learning sabse pehle maa baap se hi aati hai :-). Lots of love to your family ❤ \n {keyword}"
-            response = openai.Completion.create(
+
+@app.route('/parent/<id>', methods=['GET', 'POST'])
+@check_login_status
+def parent(id):
+    if request.method == 'GET':
+        user_chats = chats.find_one({"userid": id})
+        if user_chats is not None:
+            response_data = user_chats["chat"]
+            return jsonify({'response': response_data})
+        else:
+            return jsonify({'response': []})
+
+    if request.method == 'POST':
+        keyword = request.json.get('keyword')
+        print(keyword)
+        print(id)
+        prom = prompts.find_one({"userid": id})
+        user_chats = chats.find_one({"userid": id})
+        
+        if prom is None:
+            obj = {
+                    "userid": id,
+                    "prompt":"Act as parenting influencer. You provide solutions to all parenting problems, you have to answer in the language in which the user asks you the question. While answering to them reply as human not machine try to replicate the tone of user.\n\nPrompt: My daughter is 19 mnths. But agr usko me koi chiz nai deti hu ya uski koi zid Puri nai kti hu tou wo mushe hit kart hai She is just 19 mnths. how can I control this behaviour Ya kabhi kabhi wo masti me b mujhe hit kar deti hai.\nAnswer: - Aapki beti choti hai. Is umar mein kuch na milne pe kaise behave karna hai bachon ko pata nahin hot. Emotion pe kaabu nahin hota. Lekin bachon ka bhi maarna rok sakte hai. Thoda time laga ke. Kabhi bhi jiss cheez ke liye bacha hit kar raha hai woh puri nahin karni kyonki phir bachey ko lagta hai ke maarne se cheez milegi. So a no means a no. But pyaar se. Aap calm aawaaz mein usko bol sakti hai - No using hands and feet. Mujhe lagti hai. Same line hi humein baar baar use karni hai. Phir Aap ski feeling ko acknowledge karo. Ke aapko woh chahiye. Haan? Mujhe pata hai. Mujhe pata hai aapko aacha lagta hai. Lekin maarne se kabhi nahin milega. Aur pun nai kti hu tou Mummy loves you. Ya kabhi kabhi wo Bachon ke nervous system ko touch karke se calmness milti hai. Unko touch karke pyaar se mana karenge to baat samajne ka chance zada hai. Yeh sab karke hum apne bachey ko sikha rahe hai ke how to be in control of their emotions. Yeh important learning sabse pehle maa baap se hi aati hai :-). Lots of love to your family\nprompt:How can I help my 4-year-old daughter who is always very angry?\nAnswer:It's common for young children to experience emotions like anger. As a parent, you can help your daughter by offering understanding and patience. Show her love and empathy when she's upset, and encourage her to express her feelings through words instead of actions. Create a calm environment and teach deep breathing techniques to help her manage anger. Establish consistent routines and boundaries to provide a sense of security. Engage in fun activities together to build a strong bond. Praise positive behaviors and be a positive role model. Seek professional help if her anger becomes persistent or severe. Remember, patience and love are key in guiding her emotional development.",
+                }
+            prompts.insert_one(obj)
+        
+        main = prompts.find_one({"userid":id})
+        passing_data = main['prompt']+" " + "prompt:"+" "+ keyword
+        # print(passing_data)
+
+        try:
+            def run_prompt():
+                prompt = passing_data
+                response = openai.Completion.create(
                 engine="text-davinci-003",
                 prompt=prompt,
                 max_tokens=300,
                 temperature=0.7
+                )
+                generated_response = response.choices[0].text.strip()
+                # ... (rest of the code for the run_prompt function)
+                return generated_response
+
+            response = run_prompt()
+            savedata = passing_data+"response:"+" "+response
+            print(savedata)
+            prompts.update_one(
+                {"userid": id},
+                {"$set": {"prompt": savedata}}
             )
-            generated_response = response.choices[0].text.strip()
-            return generated_response
+            print(response)
+            if user_chats is None:
+                obj = {
+                    "userid": id,
+                    "chat": [
+                        {"text": keyword, "fromuser": True},
+                        {"text": response, "fromuser": False}
+                    ]
+                }
+                chats.insert_one(obj)
+                
 
-        response = run_prompt()
-        return jsonify({'response': response})
-    except Exception as e:
-        return str(e)
+            else:
+                chat1 = {"text": keyword, "fromuser": True}
+                chat2 = {"text": response, "fromuser": False}
+                chats.update_one(
+                    {"userid": id},
+                    {"$push": {"chat": chat1}}
+                )
+                chats.update_one(
+                    {"userid": id},
+                    {"$push": {"chat": chat2}}
+                )
 
+            user_chat_array = chats.find_one({"userid": id})
+            response_data = user_chat_array["chat"]
+            print(response_data)
+            return jsonify({'response': response_data})
 
-
-# fine tuning
-# @app.route('/parent', methods=['POST'])
-# @check_login_status
-# def parent():
-#     keyword = request.json.get('keyword')
-#     print(keyword)
-    
-#     try:
-#         def run_prompt():
-#             prompt = f"prompt: {keyword} "
-#             response = openai.Completion.create(
-#                 model="davinci:ft-personal:output-2023-07-21-15-48-23",  # Replace with your fine-tuned model ID
-#                 prompt=prompt,
-#                 max_tokens=300,
-#                 temperature=1.19,
-#                 stop=None,  # You can specify a stopping condition if needed
-#                 frequency_penalty=0.0,
-#                 presence_penalty=0.0
-#             )
-#             generated_response = response.choices[0].text.strip()
-#             return generated_response
-
-#         response = run_prompt()
-#         return jsonify({'response': response})
-#     except Exception as e:
-#         return str(e)
-    
-
-# @app.route('/parent', methods=['POST'])
-# @check_login_status
-# def test():
-#     prompt = request.json.get('prompt')
-#     # Set your API key here
-#     openai.api_key = os.getenv('OPENAI_API_KEY')
-
-#     response = openai.Completion.create(
-#         model="davinci:ft-personal:output-2023-07-21-15-48-23",  # Replace with your fine-tuned model ID
-#         prompt=prompt,
-#         max_tokens=150,  # You can adjust the max_tokens parameter as needed
-#         temperature=1,  # Adjust the temperature for more random or conservative outputs
-#         stop=None,  # You can specify a stopping condition if needed
-#         frequency_penalty=0.0,
-#         presence_penalty=0.0
-#     )
-#     generated_text = response.choices[0].text.strip()
-
-#     # Print the generated text
-#     return jsonify({"message": generated_text})
+        except Exception as e:
+            print("Error:", str(e))
+            return jsonify({'error': str(e)})
 
 
-    
+
+
 @app.route('/check')
 def tune_list():
     openai.api_key = os.getenv('OPENAI_API_KEY')
